0cb0adda8f6f336251d7481f9ae876081984c300
Philip Ford <pford68@earthlink.net>
2018-08-04 11:28:25 -0400

    First commit

---
+++ src/components/AppHeader.js
@@ -0,0 +1,21 @@
+/**
+ *
+ */
+import React from 'react';
+import {
+    AppBar,
+    Toolbar,
+    Typography,
+} from '@material-ui/core';
+
+const AppHeader = () => (
+<AppBar position="static">
+    <Toolbar>
+    <Typography variant="title" color="inherit">
+    My React App
+</Typography>
+</Toolbar>
+</AppBar>
+);
+
+export default AppHeader;
